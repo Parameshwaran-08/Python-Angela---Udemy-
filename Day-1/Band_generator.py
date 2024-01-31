e87d9b5ca50f82365a7e59d774508a4b2321c550
Parameshwaran-08 <parameshwaranayyappan08gmail.com>
2024-01-31 20:00:59 +0530

    feat : Added Band generator python file

---
+++ Day-1/Band_generator.py
@@ -0,0 +1,6 @@
+# Band name generator
+
+print("Welcome to the band name generator !")
+city = input("Enter the city you had grown up : ")
+pet = input("Enter the name of your pet : ")
+print(f"The name of your band is {city} {pet}.")
