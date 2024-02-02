1ce14db41cb8693d3aaa63ed4f97405472449ac9
Parameshwaran-08 <parameshwaranayyappan08gmail.com>
2024-02-02 20:35:03 +0530

    create : Random concept generator for revising.

---
+++ Sequence(Data_types)/concept_tester.py
@@ -0,0 +1,9 @@
+from random import choice
+
+data_types = choice(["List","Tuple","Set","Dictionary"])
+topics = choice(["access","change","update","replace","add","remove","delete","loop","comprehension","bubble sort","insertion sort","bubble sort - while loop","insertion sort - for loop","bubble sort - for loop","insertion sort - while loop","copy","join","duplicate removing","unpack"])
+
+print(f"Testing your knowledge on {data_types}({topics}).")
+
+
+
